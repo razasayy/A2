fcb49b5432ccf3d4bde51a9d88205821e61bdeec
S M Jawwad Raza <32301895+S-M-Jawwad-Raza@users.noreply.github.com>
2020-11-11 10:45:24 -0500

    Fix errors in HTML and JS so that script can run

--- A2/Ex1/ex1.js
+++ A2/Ex1/ex1.js
@@ -1,27 +1,34 @@
 document.getElementById("calculate").addEventListener("click", validator);
 
 function Ellipsoidvolume(a,b,c){
-    return Math.pi * 4/3 * a * b *c;
+    return (Math.PI * (4 / 3) * a * b *c);
 }
 
 
 function validator(){
 
-var a = document.getElementById("num1").content
-var b = document.getElementById("num2").content
-var c = document.getElementById("num3").content
+    var a = document.getElementById("num1").value
+    var b = document.getElementById("num2").value
+    var c = document.getElementById("num3").value
+    var message = "";
 
-
-    if (a >= 1 && a <= 100){
-
+    //check if inputs are numbers between 1 and 100 inclusive
+    if (!isNaN(a) || a < 1 || a > 100){
+        message += "Invalid entry for a. Please enter a number between 1 and 100\n"
+    } 
+    if (!isNaN(b) || b < 1 || b > 100){
+        message += "Invalid entry for b. Please enter a number between 1 and 100\n"
     }
-    
-    if (b >= 1 && b <=100){
-    
-    
+    if (!isNaN(c) || c < 1 || c > 100){
+        message += "Invalid entry for c. Please enter a number between 1 and 100\n"
     }
-    
-    if (c >= 1 && c <= 100){
-    
+
+    if (message != ""){
+        alert(message);
+    }
+
+    else{
+        var volume = Ellipsoidvolume(a,b,c);
+        document.getElementById("result").innerText(volume);
     }
 }
