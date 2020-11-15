ba28c9acb7a3b56b6e04a236725895bee9e450da
shehab10-boop <shehab_1297@hotmail.com>
2020-11-14 20:47:07 -0500

    inputted number not displayed & goes to next page

--- A2/Ex5/ex5.js
+++ A2/Ex5/ex5.js
@@ -1,15 +1,15 @@
 
+    document.getElementById("output").addEventListener("click", Pattern);
 
 function Pattern(){
     
-    document.getElementById("output").addEventListener("click", pattern);
 
     var num1 = document.getElementById("num1").value
 
     var i,j;
     for(i=0; i <= num1; i++) {
        
-      for(j=0; j<=i; j++) {
+      for(j=1; j<=i; j++) {
          document.write("XX "+ "");
     }
     document.write("<br/>");
